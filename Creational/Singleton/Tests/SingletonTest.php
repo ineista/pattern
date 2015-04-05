15d298ec1b5b09fb208e0f4c987219af423a944f
ineista <a_ineista20@yahoo.com>
2015-04-05 23:51:36 +0100

    Initial commit

---
+++ Creational/Singleton/Tests/SingletonTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace DesignPatterns\Creational\Singleton\Tests;
+
+use DesignPatterns\Creational\Singleton\Singleton;
+
+/**
+ * SingletonTest tests the singleton pattern
+ */
+class SingletonTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function setUp(){
+        require_once '../Singleton.php';
+    }
+
+    public function testUniqueness()
+    {
+        $firstCall = Singleton::getInstance();
+        $this->assertInstanceOf('DesignPatterns\Creational\Singleton\Singleton', $firstCall);
+        $secondCall = Singleton::getInstance();
+        $this->assertSame($firstCall, $secondCall);
+    }
+
+    public function testNoConstructor()
+    {
+        $obj = Singleton::getInstance();
+
+        $refl = new \ReflectionObject($obj);
+        $meth = $refl->getMethod('__construct');
+        $this->assertTrue($meth->isPrivate());
+    }
+}
